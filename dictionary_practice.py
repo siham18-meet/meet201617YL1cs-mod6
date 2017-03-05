2b177ec1335aeda1fa3fa36d7e24b0d51a17b55e
siham18-meet <sihamabuali.12@gmail.com>
2017-03-05 19:58:23 +0200

    I like html better

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -6,23 +6,26 @@ my_phonebook={"Statue of Liberty":2125555555, "Ghostbusters":2125551234}
 ###
 #Assign the value of the Statue of Liberty's phone number to the variable, num
 ###
-
+num= my_phonebook["Statue of Liberty"]
+print(num)
 ###
 #Print the variable, num
 ###
-
+Ghostbusters=2125559876
+print(Ghostbusters)
 ###
 #Change the value of the Ghostbusters' phone number to 2125559876
 ###
 
 #Here's an empty dictionary
-your_phonebook={}
-
+your_phonebook={"Linda":05473548496,"Layan":05283454989,"Salma":05036748784,"Sara":052746485856,"Hala":054376746575}
+#your_phonebook["Linda"]=05473548496
+your_phonebook.keys()
+for key in sequence_of_keys :
 ###
 #Add 5 values to it like this
 #your_phonebook['key']=value
 ###
-
 ###
 #Use the keys() method: your_phonebook.keys()
 #It will produce a sequence of all the keys
